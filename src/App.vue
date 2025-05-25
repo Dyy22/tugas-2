d8e777e337df107715b5915a3e0e0e9f071a5c51
Muhammad Perdiansyah <muhammad.perdiansyah@inteleq.id>
2025-05-25 13:01:15 +0700

    finishing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app-container">
+    <div class="quote-box">
+      <div class="quote-content">
+        <span class="quote-icon">🙶</span>
+        <p class="quote-text">{{ text }}</p>
+      </div>
+      <div class="quote-author">
+        - {{ author }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: "Selamat pagi tutor, ini adalah tugas kedua saya",
+      author: "Muhammad Perdiansyah"
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url("https://fonts.googleapis.com/css?family=Raleway:400,500");
+  
+  body {
+    font-family: Raleway, sans-serif;
+    margin: 0;
+  }
+
+  #app-container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: orange;
+  }
+
+  .quote-box {
+    background-color: white;
+    padding: 50px;
+    max-width: 800px;
+    border-radius: 5px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+  }
+
+  .quote-content {
+    display: flex;
+    gap: 20px;
+  }
+
+  .quote-icon {
+    font-size: 90px;
+    color: #555;
+  }
+
+  .quote-text {
+    color: orange;
+    font-size: 40px;
+  }
+
+  .quote-author {
+    display: flex;
+    justify-content: flex-end;
+    font-size: 20px;
+    color: #555;
+  }
 </style>
